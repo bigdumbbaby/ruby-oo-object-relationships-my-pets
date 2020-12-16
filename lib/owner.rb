a0a314ba29f0451672d82269e8a217a6b262ebbd
Colton <colton.shawn.oconnor@gmail.com>
2020-12-16 12:21:14 -0700

    commit tree 29e7445b5b471c35fb423d8d8a6aa3d9d5f4ba47

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,45 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize name
+    @name = name
+    @species = "human"
+
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.count 
+    all.count
+  end
+
+  def self.reset_all 
+    @@all = []
+  end
+
+  def self.all 
+    @@all 
+  end
+
+
+  def buy_cat name 
+    Cat.new "name", self
+  end
+
+  def buy_dog name 
+    Dog.new "name", self
+  end
+
+  # def walk_dogs
+  # end
+
+  # def walk_cats
+  # end
+
 end
